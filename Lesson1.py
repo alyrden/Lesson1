73fb79bace71ba08a92369eb59d2def490230d60
alyrden <f50knox@gmail.com>
2024-08-06 00:21:10 +0300

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,13 @@
+# Задача1
+print("1st program")
+print(float(9 ** 0.5) * 5)
+# Задача2
+print("2nd program")
+print((9.99 > 9.98 and 9.99 !=1000.1))
+# Задача3
+print("3nd program")
+print ((2*2+2)==(2* (2+2)))
+# Задача4
+print("4nd program")
+x = float('123.456')
+print(int(x * 10) % 10)
